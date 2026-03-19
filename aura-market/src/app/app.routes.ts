2630a50000c64d9ef4bf7536939f73d6bb1aa424
Roberto Jara <devjara16@gmail.com>
2026-03-18 21:44:26 -0600

    fix(component) Corrección estructural entre componentesde carpetas entr e

--- aura-market/src/app/app.routes.ts
+++ aura-market/src/app/app.routes.ts
@@ -1,16 +1,15 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from './pages/home.component';
-import { CatalogComponent } from './pages/catalog.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
 import { LayoutComponent } from './layouts/layout.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    component: LayoutComponent, 
+    component: LayoutComponent,
     children: [
       { path: '', component: HomeComponent, title: 'Aura Market | Inicio' },
       { path: 'catalogo', component: CatalogComponent, title: 'Aura Market | Catálogo' },
-      // Aquí agregaremos la ruta del producto: { path: 'producto/:id', component: ProductDetailComponent }
     ]
   }
 
